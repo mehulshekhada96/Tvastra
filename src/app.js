73bfe70b6fdee5b518d576adf1fd55c04e96d9bb
MehulShekhada <mehulshekhada9@gmail.com>
2021-07-13 20:06:35 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ app.use(session({
 app.use(express.static(path.join(__dirname,"client")));
 
 // Public Folder
-app.use(express.static(path.join(__dirname, './client/public')));
+// app.use(express.static(path.join(__dirname, '/client/public')));
 
 app.use(cors());
 app.use(compression());
@@ -54,7 +54,7 @@ app.set("port",process.env.PORT || 5000)
 
 let mongoose = require('mongoose');
 
-const uri = "mongodb+srv://Tvastra:sgoc.2030@cluster0.w2dnb.mongodb.net/Tvastra?retryWrites=true&w=majority";
+const uri = "mongodb+srv://Tvastra:Sgoc.2030@cluster0.w2dnb.mongodb.net/Tvastra?retryWrites=true&w=majority";
 
 mongoose.connect(uri, 
     {useNewUrlParser: true, 
